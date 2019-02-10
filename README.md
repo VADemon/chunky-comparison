a675503e928d081a2dfd3b74b9d5198c5eafad4f
VADemon <wademon@yandex.ru>
2019-02-10 03:11:03 +0100

    Edited JS, added all images. wish me luck

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-yolo-octo-bugfixes
+https://VADemon.github.io/yolo-octo-bugfixes/
+
+Chunky render comparison
 ==================
+
+Thanks to this page: https://xooyoozoo.github.io/yolo-octo-bugfixes/
+I hacked'n'slahed it to work.
--- js/splitimage2.js
+++ js/splitimage2.js
@@ -23,26 +23,6 @@
     selCandidates[Math.floor(Math.random() * selCandidates.length)].selected = true;
 })();
 
-var workers = {
-    bpg: undefined,
-    jp2: undefined, jxr: undefined, webp: undefined
-};
-var nativeDec = {
-    bpg: false, jp2: false, jxr: false, webp: false,
-    check: function(flag, decodedWidth, encodedUrl) {
-        var supports = this;
-        var img = new Image();
-        img.src = encodedUrl;
-        img.onload = img.onerror = function() {
-            supports[flag] = (img.width && img.width === decodedWidth);
-        }
-    }
-};
-
-nativeDec.check('jp2', 4, 'data:image/jp2;base64,AAAADGpQICANCocKAAAAFGZ0eXBqcDIgAAAAAGpwMiAAAAAtanAyaAAAABZpaGRyAAAABAAAAAQAAw8HAAAAAAAPY29scgEAAAAAABAAAABpanAyY/9P/1EALwAAAAAABAAAAAQAAAAAAAAAAAAAAAQAAAAEAAAAAAAAAAAAAw8BAQ8BAQ8BAf9SAAwAAAABAQAEBAAB/1wABECA/5AACgAAAAAAGAAB/5PP/BAQFABcr4CA/9k=');
-nativeDec.check('jxr', 1, 'data:image/vnd.ms-photo;base64,SUm8AQgAAAAFAAG8AQAQAAAASgAAAIC8BAABAAAAAQAAAIG8BAABAAAAAQAAAMC8BAABAAAAWgAAAMG8BAABAAAAHwAAAAAAAAAkw91vA07+S7GFPXd2jckNV01QSE9UTwAZAYBxAAAAABP/gAAEb/8AAQAAAQAAAA==');
-nativeDec.check('webp', 2, 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA');
-
 var select = {
     file: getElId('fileSel'), scale: getElId('scaleSel'),
     left: getElId('leftSel'), right: getElId('rightSel')
@@ -117,11 +97,9 @@ select.file.onchange = function() {
 select.scale.onchange = processCanvasScale;
 
 select.left.onchange = function() {
-    checkWorkers('left');
     setSide('left');
 };
 select.right.onchange = function() {
-    checkWorkers('right');
     setSide('right');
 };
 
@@ -157,28 +135,6 @@ function getSlugName(str) {
     .replace(/-+/g, '-'); // collapse dashes
 
     return str;
-}
-
-/* Create new worker if needed. Terminate worker if unneeded. */
-function checkWorkers(sel) {
-    var curSel = getSelValue(select[sel], 'value');
-    var img = { l: getSelValue(select.left, 'value'),
-                r: getSelValue(select.right, 'value') };
-
-    processWorker('jp2');
-    processWorker('jxr');
-    processWorker('webp');
-
-    function processWorker(codec) {
-        if (img.l != codec && img.r != codec) {
-            if (workers[codec] !== undefined) {
-                workers[codec].terminate();
-                workers[codec] = undefined;
-            }
-        } else if (!nativeDec[codec] && curSel==codec && workers[codec]===undefined) {
-            workers[codec] = new Worker('js/' + codec + 'Worker.js');
-        }
-    }
 }
 
 /* Uses Lanczos2 for rescaling. In-browser too blurry. Lanczos3 too slow. */
@@ -289,7 +245,8 @@ function setImage(side, pathBase, codec, setText) {
     };
     view[side].style.opacity = 0.5;
 
-    var path = urlFolder.concat(pathBase, '/', urlFile, '.', codec);
+	var filename = getSelValue(select[side], 'fname');
+    var path = urlFolder.concat(pathBase, '/', filename, '.', codec);
     var xhr = new XMLHttpRequest();
 
     xhr.open("GET", path, true);
@@ -307,130 +264,30 @@ function setImage(side, pathBase, codec, setText) {
         var canvas = canvases[side];
         canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
 
-        if (codec == 'bpg') {
-            var bpg = new BPGDecoder(canvas.getContext("2d"));
-            bpg.onload = function() {
-                canvas.width = bpg.imageData.width;
-                canvas.height = bpg.imageData.height;
-                canvas.getContext("2d").putImageData(bpg.imageData, 0, 0);
-                processCanvasScale(canvas, side);
-                window.URL.revokeObjectURL(blobPath);
-            };
-            bpg.load(blobPath);
-        } else {
-            var image = new Image();
+        var image = new Image();
             image.onload = function() {
-                canvas.width = image.width;
-                canvas.height = image.height;
-                canvas.getContext("2d").drawImage(image, 0, 0);
-                processCanvasScale(canvas, side);
-                window.URL.revokeObjectURL(blobPath);
-            };
-            image.onerror = function() {
-                var arrayData = new Uint8Array(xhr.response);
-
-                if (codec == 'jp2' || codec == 'j2k') {
-                    /* JPEG 2000 decoding */
-                    j2kArrayToCanvas(arrayData, codec, canvas);
-                } else if (codec == 'jxr') {
-                    /* JPEG XR decoding */
-                    workers.jxr.onmessage = function(event) {
-                        var jxrBmp = new Blob([new DataView(event.data.buffer)], {type: "image/bmp"});
-                        jxrBmpToCanvas(jxrBmp, canvas);
-                    };
-                    if (workers.jxr !== undefined) { workers.jxr.postMessage(xhr.response); }
-                    else console.error("Cannot decode JPEG XR.");
-                } else if (codec == 'webp') {
-                    /* WebP decoding */
-                    webpArrayToCanvas(arrayData, canvas);
-                } else { console.error("No support for " + url); }
-            };
-            image.src = blobPath;
-        }
+            canvas.width = image.width;
+            canvas.height = image.height;
+            canvas.getContext("2d").drawImage(image, 0, 0);
+            processCanvasScale(canvas, side);
+            window.URL.revokeObjectURL(blobPath);
+        };
+        image.onerror = function() {
+            console.error("No support for " + url);
+        };
+        image.src = blobPath;
     };
     xhr.send();
-
-	function j2kArrayToCanvas(encData, codec, canvas) {
-	    workers.jp2.onmessage = function(event) {
-	        var bitmap = event.data.data;
-	        if (!bitmap) { return false; }
-
-	        canvas.width = event.data.width;
-	        canvas.height = event.data.height;
-	        var ctx = canvas.getContext("2d");
-	        var output = ctx.createImageData(canvas.width, canvas.height);
-            var outputData = output.data;
-
-            var pixelsPerChannel = canvas.width * canvas.height;
-	        var i = 0,
-	            j = 0;
-	        while (i < outputData.length && j < pixelsPerChannel) {
-	            outputData[i] = bitmap[j]; // R
-	            outputData[i + 1] = bitmap[j + pixelsPerChannel]; // G
-	            outputData[i + 2] = bitmap[j + (2 * pixelsPerChannel)]; // B
-	            outputData[i + 3] = 255; // A
-	            i += 4;
-	            j += 1;
-	        };
-	        ctx.putImageData(output, 0, 0);
-	        processCanvasScale(canvas, side);
-	    };
-	    if (workers.jp2 !== undefined) {
-	        workers.jp2.postMessage({
-	            bytes: encData,
-	            extension: codec
-	        });
-	    } else console.error("Cannot decode JPEG 2000.");
-	};
-    function jxrBmpToCanvas(jxrBmp, canvas) {
-        var bmpUrl = window.URL.createObjectURL(jxrBmp);
-        var img = new Image();
-        img.onload = function() {
-            canvas.width = img.width;
-            canvas.height = img.height;
-            canvas.getContext("2d").drawImage(img, 0, 0);
-            processCanvasScale(canvas, side);
-            window.URL.revokeObjectURL(bmpUrl);
-        };
-        img.src = bmpUrl;
-    };
-	function webpArrayToCanvas(encData, canvas) {
-	    workers.webp.onmessage = function(event) {
-	        var bitmap = event.data.bitmap;
-	        var biWidth = event.data.width;
-	        var biHeight = event.data.height;
-	        if (!bitmap) { return false; }
-
-	        canvas.width = biWidth;
-	        canvas.height = biHeight;
-	        var ctx = canvas.getContext("2d");
-	        var output = ctx.createImageData(canvas.width, canvas.height);
-	        var outputData = output.data;
-
-	        for (var h = 0; h < biHeight; h++) {
-	            for (var w = 0; w < biWidth; w++) {
-	                outputData[0 + w * 4 + (biWidth * 4) * h] = bitmap[1 + w * 4 + (biWidth * 4) * h];
-	                outputData[1 + w * 4 + (biWidth * 4) * h] = bitmap[2 + w * 4 + (biWidth * 4) * h];
-	                outputData[2 + w * 4 + (biWidth * 4) * h] = bitmap[3 + w * 4 + (biWidth * 4) * h];
-	                outputData[3 + w * 4 + (biWidth * 4) * h] = bitmap[0 + w * 4 + (biWidth * 4) * h];
-	            };
-	        };
-	        ctx.putImageData(output, 0, 0);
-	        processCanvasScale(canvas, side);
-	    };
-	    if (workers.webp !== undefined) {
-	        workers.webp.postMessage(encData);
-	    } else console.error("Cannot decode WebP.");
-	}
 }
 
 function setSide(side) {
 	var isRight = (side == 'right') ? 1 : 0;
 	var whichQual = (isRight) ? rightQual : leftQual;
     var image = getSelValue(select[side], 'value');
-    var pathBase = getSelValue(select[side], 'folder');
+	// scene/crop folder
+    var pathBase = getSelValue(select.file, 'value');
 
-    if (pathBase != 'Original' && urlFolder != 'psycomp/') {
+    if (false) { //(pathBase != 'Original') {
         whichQual.disabled=false;
         var quality = whichQual.options[whichQual.selectedIndex].innerHTML.toLowerCase() + '/';
     } else {
@@ -451,20 +308,11 @@ function setSide(side) {
 
 function setFile() {
     urlFile = getSelValue(select.file, 'value');
-    /*if (urlFile === 'random') {
-        var chosen = getRandExtra();
-
-        urlFile = chosen.name;
-        urlFolder = 'randomextras/';
-
-        select.file.options[select.file.options.length - 2].selected = true;
-    }*/
 
     /* Flag for special processing when both left & right are both new. */
     first = 1;
     /* Any view change will update hash. */
     viewOptions.file = getSlugName(select.file.options[select.file.selectedIndex].text);
-    if (urlFolder == 'psycomp/') { viewOptions.file = 'psycomp#' + viewOptions.file; }
 
     setSide('right');
     setSide('left');
@@ -526,26 +374,6 @@ function getWindowsOptions() {
         var hashArr, ampArr, imgOpts, name, scale, leftOpts, rightOpts;
 
         hashArr = (location.hash).split('#', 3);
-        if (hashArr[1]) {
-            if (hashArr[1] == "psycomp") {
-                urlFolder = "psycomp/";
-                preparePsyCompSelects();
-                prepareEncodeDesc("psycomp");
-            } else if (hashArr[1] == "addpsy") {
-                var psyOpt  = new Option("BPG-psy", "bpg");
-                psyOpt.setAttribute("folder", 'BPG-psy');
-                select.left.insertBefore(psyOpt.cloneNode(true),
-                                         select.left.firstChild);
-                select.right.insertBefore(psyOpt,
-                                         select.right.firstChild);
-                select.file.options[27].selected = true;
-                select.left.options[1].selected = true;
-                select.right.options[0].selected = true;
-                leftQual.options[1].selected = true;
-                rightQual.options[1].selected = true;
-                prepareEncodeDesc("addpsy");
-            }
-        }
 
         ampArr = (hashArr.pop()+'&='+'&=').split('&', 5);
 
@@ -562,61 +390,25 @@ function getWindowsOptions() {
                     var z = document.querySelector('#scaleSel [ratio="' + imgOpts[1] + '"]');
                     if (z) {z.selected = true};
                 }
-                if (urlFolder == "psycomp") { break; }
+                
                 if (leftOpts) {
                     s = document.querySelector('#leftSel [value="' + leftOpts[0] + '"]');
                     if (s) {s.selected = true};
                     q = document.querySelector('#leftQual [value="' + leftOpts[1] + '"]');
                     if (q) {q.selected = true};
-                    checkWorkers('left');
                 }
                 if (rightOpts) {
                     s = document.querySelector('#rightSel [value="' + rightOpts[0] + '"]');
                     if (s) {s.selected = true};
                     q = document.querySelector('#rightQual [value="' + rightOpts[1] + '"]');
                     if (q) {q.selected = true};
-                    checkWorkers('right');
                 }
                 break;
             }
         };
     };
 
-    function preparePsyCompSelects() {
-        select.left.options.length = 0;
-        leftQual.options[2].selected = true;
-        leftQual.disabled = true;
-        var soft = new Option("BPG-soft", "bpg");
-        soft.setAttribute("folder", 'BPG-soft');
-        select.left.appendChild(soft);
 
-        select.right.options.length = 0;
-        rightQual.options[2].selected = true;
-        rightQual.disabled = true;
-        var psy  = new Option("BPG-psy", "bpg");
-        psy.setAttribute("folder", 'BPG-psy');
-        select.right.appendChild(psy);
-    }
-    function prepareEncodeDesc(comp) {
-        var clis, encs;
-
-        clis = getElId("descCli");
-        encs = getElId("descEnc");
-
-        if (comp == "psycomp"){
-            clis.innerHTML = "";
-            encs.innerHTML = "";
-
-            insertText(clis, "BPG-soft: bpgenc-0.9.4 -b 10 -m 9, with x265-1.4+226 --no-cutree --aq-mode 1 --crf");
-            insertText(clis, "BPG-psy: [...] --deblock -2:-2 --{cbqpoffs, crqpoffs} -1 --psy-rd 0.8 --psy-rdoq 2.4 --bitrate");
-
-            insertText(encs, "Soft 1st encoded to equivalent of 28 base QP. Psy matched to within +/- 5% filesize.");
-            insertText(encs, "Press Shift to flip between individual encodes. Rescaling is through Lanczos2.");
-        } else if (comp == "addpsy") {
-            insertText(clis, "bpgenc-0.9.4 -b 10 -m 9, with x265-1.4+228 --aq-mode 1 --psy-rd [0.25,0.5,0.75,1] --psy-rdoq [1,1.6,2.2,2.8] --deblock -2:-2 --{cbqpoffs, crqpoffs} -1",
-                clis.firstChild);
-        }
-    }
     function insertText(el, text, pushedBackText) {
         var p = document.createElement("p");
         var textNode = document.createTextNode(String(text));
@@ -634,18 +426,6 @@ getWindowsOptions();
 
 window.addEventListener("load", function() {
     setFile();
-    /*loadScript('randomextras/extras.js', function() {
-        var randOpt = new Option("Random", "random");
-        var blank = document.createElement("option");
-        var optGroup = document.createElement("optgroup");
-
-        blank.disabled = true;
-        optGroup.setAttribute("label", 'More?');
-
-        optGroup.appendChild(randOpt);
-        select.file.appendChild(blank);
-        select.file.appendChild(optGroup);
-    });*/
 }, false);
 window.addEventListener("keydown", function(event) {
     /*if (event.keyCode == "82") {
